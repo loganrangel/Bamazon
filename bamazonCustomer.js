3d75d42a8f5d607034a15183188bd415f50f43ba
Logan Angel <you@example.com>
2019-06-06 16:20:11 -0400

    functional inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,10 +5,28 @@ var connection = mysql.createConnection ({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Broketoe1.",
     database: "bamazon"
 });
 
+connection.connect(function(err) {
+    if (err) throw err;
+    promptPurchase();
+  });
+
 function promptPurchase() {
-    
+    console.log("it works");
+    inquirer.prompt([{
+        name: "Choices",
+        type: "list",
+        message: "What item would you like to purchase?",
+        choices: ["tent", "sleeping bag", "backpack", "gas stove", "folding chair", ]
+    },
+    {
+        name: "quantity",
+        type: "input",
+        message: "How many of this item do you need?",
+        filter: Number
+    }
+]).then
 }
